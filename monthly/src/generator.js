ca4bf2c2b218ab6407a1a49db3d9ce12aefb1e94
Gokhan Gorali <Gokhan.Gorali@telenity.com>
2017-01-28 22:13:02 +0200

    2013 page. Bandcamp tracks grid generator

---
+++ monthly/src/generator.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+
+   $('#title').load('htm/title.html');
+   $('#popup').load('htm/popup.html');
+
+    var pre = "<iframe style='border: 0; width: 300px; height: 300px;' src='http://bandcamp.com/EmbeddedPlayer/album=2503061837/size=large/bgcol=ffffff/linkcol=0687f5/t=";
+    var post = "/transparent=true/' seamless><a href='http://nekropsi.bandcamp.com/album/aylik-monthly/'>AYLIK / MONTHLY by Nekropsi</a></iframe>";
+    var map = {
+        "1": "Ocak/January",
+        "2": "<a href=\"4-4/index.html\">Şubat/February</a>",
+        "3": "Mart/March",
+        "4": "Nisan/April",
+        "5": "Mayıs/May",
+        "6": "Temmuz/July",
+        "7": "Ağustos/August",
+        "8": "Eylül/September",
+        "9": "Kasım/November",
+        "10": "Aralık/December",
+        "11": "Özet/Summary"
+    };
+    for (var i = 11; i >= 1; i--) {
+        $('#tracks').append("<div class=\"span4\">"  + "</p>" + pre + i + post+ "<p class=\"lead text-right\">" + map[i]);
+    };
+});
+
+
+
